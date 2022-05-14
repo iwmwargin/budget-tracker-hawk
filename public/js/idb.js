0cc3c9a8ead3b06dac2ba27ae19566fca5da341b
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-05-14 19:39:09 -0400

    created service worker file

--- public/js/idb.js
+++ public/js/idb.js
@@ -30,3 +30,52 @@ request.onerror = function(event) {
     // log error here
     console.log(event.target.errorCode);
 }
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+
+    const budgetObjectStore = transaction.objectStore("new_transaction");
+
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+
+    const transaction = (["new_transaction"], "readwrite");
+
+    const budgetObjectStore = transaction.objectStore("new_transaction");
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(["new_transaction"], "readwrite");
+
+                const budgetObjectStore = transaction.objectStore("new_transaction");
+
+                budgetObjectStore.clear();
+
+                alert("All saved transaction has been submitted!");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+}
+
+window.addEventListener("online", uploadTransaction);
